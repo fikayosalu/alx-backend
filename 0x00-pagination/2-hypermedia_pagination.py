4dbdd640ccd24d71c3bd269eb9bac8a64c8b6c6a
fikayosalu <salufikayo@gmail.com>
2025-05-13 04:15:32 +0100

    Task 2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+"""Contains a class Server that handles pagination"""
 import csv
 import math
-from typing import List
+from typing import List, Iterator, Union, Dict, Any
 
 
 class Server:
@@ -9,44 +10,43 @@ class Server:
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
-    def __init__(self):
-        self.__dataset = None
+    def __init__(self) -> None:
+        self.__dataset: Union[None, List[List]] = None
 
     def dataset(self) -> List[List]:
         """Cached dataset
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
-                reader = csv.reader(f)
-                dataset = [row for row in reader]
+                reader: Iterator[List[str]] = csv.reader(f)
+                dataset: List[List[str]] = [row for row in reader]
             self.__dataset = dataset[1:]
 
         return self.__dataset
 
     @staticmethod
     def index_range(page: int, page_size: int) -> tuple:
-        start = (page - 1) * page_size
-        end = start + page_size
+        """Returns the sart and end range of each page"""
+        start: int = (page - 1) * page_size
+        end: int = start + page_size
         return (start, end)
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert type(page) == int and page > 0,"page must be an\
-                integer greater than 0"
-        assert type(page_size) == int and page_size > 0, "page size \
-                must be an integer greater than 0"
+        """Returns the Dataset for each page"""
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
         start, end = self.index_range(page, page_size)
         try:
             return self.dataset()[start:end]
-        except:
+        except IndexError:
             return []
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert type(page) == int and page > 0,"page must be an\
-                integer greater than 0"
-        assert type(page_size) == int and page_size > 0, "page size \
-                must be an integer greater than 0"
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+        """Returns a hypermedia for the pagination"""
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
         start, end = self.index_range(page, page_size)
-        total = math.ceil(len(self.dataset())/page_size)
+        total: int = math.ceil(len(self.dataset())/page_size)
         return {
                     "page_size": page_size,
                     "page": page,
@@ -54,4 +54,4 @@ class Server:
                     "next_page": page + 1 if page < total else None,
                     "prev_page": page - 1 if page > 1 else None,
                     "total_pages": total
-                    }
+                }
